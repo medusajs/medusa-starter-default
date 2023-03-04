1dfe69a4506880764e45917a8612fef7461321c4
adil <68734155+adevinwild@users.noreply.github.com>
2023-03-04 17:55:47 +0100

    chore: :label: Better export path
    Exporting directly from the `@medusajs/medusa` package
    Based on this discussion : https://github.com/medusajs/medusa/discussions/3275

--- medusa-config.js
+++ medusa-config.js
@@ -53,7 +53,7 @@ const plugins = [
   //   },
   // },
 ];
-/** @type {import('@medusajs/medusa/dist/types/global').ConfigModule} */
+/** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
   projectConfig: {
     // redis_url: REDIS_URL,
