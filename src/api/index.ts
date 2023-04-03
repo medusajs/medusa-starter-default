09952be93194cc5c8ec32b426797691eaaa93418
adrien2p <adrien.deperetti@gmail.com>
2023-04-03 16:06:19 +0200

    move to readme and remove components

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,6 @@
 import { Router } from "express"
-import { getConfigFile } from "medusa-core-utils"
-import { getStoreRouter } from "./routes/store"
-import { ConfigModule } from "@medusajs/medusa/dist/types/global";
 
 export default (rootDirectory: string): Router | Router[] => {
-  const { configModule: {projectConfig} } = getConfigFile(
-    rootDirectory,
-    "medusa-config"
-  ) as { configModule: ConfigModule }
-
-  const storeCorsOptions = {
-    origin: projectConfig.store_cors.split(","),
-    credentials: true,
-  }
-
-  const storeRouter = getStoreRouter(storeCorsOptions)
-
-  return [storeRouter]
+  // add your custom routes here
+  return []
 }
