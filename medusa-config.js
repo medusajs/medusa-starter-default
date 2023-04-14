4eabee4d1506ae0157d2f4619cf510fe523777b7
Emiliano Parizzi <412489+empz@users.noreply.github.com>
2023-04-14 05:32:36 +0000

    Fix jwt and cookie secret config keys.

--- medusa-config.js
+++ medusa-config.js
@@ -1,3 +1,4 @@
+//@ts-check
 const dotenv = require("dotenv");
 
 let ENV_FILE_NAME = "";
@@ -19,7 +20,7 @@ switch (process.env.NODE_ENV) {
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) {}
+} catch (e) { }
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
@@ -31,6 +32,10 @@ const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
 const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+// Set these environment variables to strong secrets for production
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
+const COOKIE_SECRET = process.env.COOKIE_SECRET || "secret";
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -62,10 +67,11 @@ const modules = {
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  jwtSecret: process.env.JWT_SECRET,
-  cookieSecret: process.env.COOKIE_SECRET,
+  jwt_secret: JWT_SECRET,
+  cookie_secret: COOKIE_SECRET,
   database_database: "./medusa-db.sql",
   database_type: DATABASE_TYPE,
+  database_logging: ["info"],
   store_cors: STORE_CORS,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
@@ -82,5 +88,5 @@ if (DATABASE_URL && DATABASE_TYPE === "postgres") {
 module.exports = {
   projectConfig,
   plugins,
-	modules,
+  modules,
 };
