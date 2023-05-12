ffc178f27527177073e9ee488f22325b8e9a5aec
olivermrbl <oliver@mrbltech.com>
2023-05-12 15:07:17 +0200

    tweak db config

--- medusa-config.js
+++ medusa-config.js
@@ -29,7 +29,7 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_TYPE = process.env.DATABASE_TYPE || "postgres";
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/vercel-commerce";
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
