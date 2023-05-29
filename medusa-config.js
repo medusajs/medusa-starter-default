4cf1c39f337fed4338a639122382466c198af402
olivermrbl <oliver@mrbltech.com>
2023-05-29 20:33:38 +0200

    add db type to template

--- medusa-config.js
+++ medusa-config.js
@@ -30,7 +30,7 @@ const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-db";
-  
+
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
