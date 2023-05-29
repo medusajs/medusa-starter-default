1876d5af33683cf96a1c20363e08fd7e7aef8c63
olivermrbl <oliver@mrbltech.com>
2023-05-29 17:30:07 +0200

    Prep for next

--- medusa-config.js
+++ medusa-config.js
@@ -28,20 +28,23 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/medusa-db";
+  
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-    },
-  },
+  `@medusajs/file-local`,
+  // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
+  // {
+  //   resolve: "@medusajs/admin",
+  //   /** @type {import('@medusajs/admin').PluginOptions} */
+  //   options: {
+  //     autoRebuild: true,
+  //   },
+  // },
 ];
 
 const modules = {
@@ -57,30 +60,22 @@ const modules = {
       redisUrl: REDIS_URL
     }
   },*/
-}
+};
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
   jwtSecret: process.env.JWT_SECRET,
   cookieSecret: process.env.COOKIE_SECRET,
-  database_database: "./medusa-db.sql",
-  database_type: DATABASE_TYPE,
   store_cors: STORE_CORS,
+  database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
-}
-
-if (REDIS_URL) {
-  projectConfig.redis_url = REDIS_URL;
-}
-
-if (DATABASE_URL) {
-  projectConfig.database_url = DATABASE_URL;
-}
-
+  // Uncomment the following lines to enable REDIS
+  // redis_url: REDIS_URL
+};
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
   projectConfig,
   plugins,
-	modules,
+  modules,
 };
