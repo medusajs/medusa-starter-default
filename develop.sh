1d9d020cf11b09f746a859683cd1baa2cab1ccdf
Lucas Fuoco <fuoco.appdev@gmail.com>
2023-06-15 11:35:54 +0400

    Remove module from develop.sh

--- develop.sh
+++ develop.sh
@@ -3,7 +3,5 @@
 #Run migrations to ensure the database is updated
 medusa migrations run
 
-node ./node_modules/@medusajs/medusa/dist/scripts/migrate-inventory-items.js
-
 #Start development environment
 medusa develop
