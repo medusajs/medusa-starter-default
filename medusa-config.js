86d2f2f93f6a99521e3a86ba54b33895939fcc9a
Lucas Fuoco <fuoco.appdev@gmail.com>
2023-06-17 11:22:23 +0400

    Add send grid template ids

--- medusa-config.js
+++ medusa-config.js
@@ -47,6 +47,19 @@ const plugins = [
     options: {
       api_key: process.env.SENDGRID_API_KEY,
       from: process.env.SENDGRID_FROM,
+      order_placed_template: process.env.SENDGRID_ORDER_PLACED_ID,
+      order_canceled_template: process.env.SENDGRID_ORDER_CANCELED_ID,
+      order_shipped_template: process.env.SENDGRID_ORDER_SHIPPED_ID,
+      order_return_requested_template: process.env.SENDGRID_ORDER_RETURN_REQUESTED_ID,
+      order_items_returned_template: process.env.SENDGRID_ORDER_ITEMS_RETURNED_ID,
+      claim_shipment_created_template: process.env.SENDGRID_CLAIM_SHIPMENT_CREATED_ID,
+      swap_created_template: process.env.SENDGRID_SWAP_CREATED_ID,
+      swap_shipment_created_template: process.env.SENDGRID_SWAP_SHIPMENT_CREATED_ID,
+      swap_received_template: process.env.SENDGRID_SWAP_RECEIVED_ID,
+      gift_card_created_template: process.env.SENDGRID_GIFT_CARD_CREATED_ID,
+      customer_password_reset_template: process.env.SENDGRID_CUSTOMER_PASSWORD_RESET_ID,
+      user_password_reset_template: process.env.SENDGRID_USER_PASSWORD_RESET_ID,
+      medusa_restock_template: process.env.SENDGRID_MEDUSA_RESTOCK_ID
     }
   },
   {
