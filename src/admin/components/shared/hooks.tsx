25d3560f801c68f5bb0492ca8c8779c0535e3604
Kasper <kasper@medusajs.com>
2023-07-06 11:50:33 +0200

    update versions

--- src/admin/components/shared/hooks.tsx
+++
@@ -1,8 +0,0 @@
-import { createCustomAdminHooks } from "medusa-react";
-
-const {
-  useAdminEntity: useAdminOnboardingState,
-  useAdminUpdateMutation: useAdminUpdateOnboardingStateMutation,
-} = createCustomAdminHooks("onboarding", "onboarding_state");
-
-export { useAdminOnboardingState, useAdminUpdateOnboardingStateMutation };
