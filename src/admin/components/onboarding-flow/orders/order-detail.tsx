0fb5ca2ab33f8ce7a3645c09b58ba9b54cedd42d
Shahed nasser <shahednasser@gmail.com>
2023-08-25 16:39:45 +0300

    added new card to onboarding

--- src/admin/components/onboarding-flow/orders/order-detail.tsx
+++ src/admin/components/onboarding-flow/orders/order-detail.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import IconBadge from "../../shared/icon-badge";
 import ComputerDesktopIcon from "../../shared/icons/computer-desktop-icon";
 import DollarSignIcon from "../../shared/icons/dollar-sign-icon";
+import ToolsSolidIcon from "../../shared/icons/tools-solid";
 
 const OrderDetail = () => {
   return (
@@ -18,13 +19,14 @@ const OrderDetail = () => {
         Medusa is a completely customizable commerce solution. We've curated
         some essential guides to kickstart your development with Medusa.
       </p>
-      <div className="grid grid-cols-2 gap-4 mt-5 pb-5 mb-5 border-b border-gray-300 border-solid">
+      <div className="grid grid-cols-3 gap-4 mt-5 pb-5 mb-5 border-b border-gray-300 border-solid auto-rows-fr">
         <a
           href="https://docs.medusajs.com/starters/nextjs-medusa-starter?path=simple-quickstart"
           target="_blank"
+          className="flex"
         >
           <div
-            className="p-4 rounded-rounded flex items-center bg-slate-50"
+            className="p-4 rounded-rounded flex items-start bg-slate-50"
             style={{
               boxShadow:
                 "0px 0px 0px 1px rgba(17, 24, 28, 0.08), 0px 1px 2px -1px rgba(17, 24, 28, 0.08), 0px 2px 4px rgba(17, 24, 28, 0.04)",
@@ -40,8 +42,7 @@ const OrderDetail = () => {
                 Start Selling in 3 Steps
               </p>
               <p className="text-xs">
-                Go live with a backend, an admin,
-                <br /> and a storefront in three steps.
+                Go live with a backend, an admin, and a storefront by following these three steps.
               </p>
             </div>
           </div>
@@ -49,9 +50,10 @@ const OrderDetail = () => {
         <a
           href="https://docs.medusajs.com/recipes/?ref=onboarding"
           target="_blank"
+          className="flex"
         >
           <div
-            className="p-4 rounded-rounded items-center flex bg-slate-50"
+            className="p-4 rounded-rounded items-start flex bg-slate-50"
             style={{
               boxShadow:
                 "0px 0px 0px 1px rgba(17, 24, 28, 0.08), 0px 1px 2px -1px rgba(17, 24, 28, 0.08), 0px 2px 4px rgba(17, 24, 28, 0.04)",
@@ -64,11 +66,37 @@ const OrderDetail = () => {
             </div>
             <div>
               <p className="font-semibold text-gray-700">
-                Build a Custom Commerce Application
+                Build Custom Use Cases
               </p>
               <p className="text-xs">
-                Learn how to build a marketplace, subscription-based
-                <br /> purchases, or your custom use-case.
+                Learn how to build a marketplace, subscription-based purchases, or your custom use-case.
+              </p>
+            </div>
+          </div>
+        </a>
+        <a
+          href="https://docs.medusajs.com/beta/?ref=onboarding"
+          target="_blank"
+          className="flex"
+        >
+          <div
+            className="p-4 rounded-rounded flex items-start bg-slate-50"
+            style={{
+              boxShadow:
+                "0px 0px 0px 1px rgba(17, 24, 28, 0.08), 0px 1px 2px -1px rgba(17, 24, 28, 0.08), 0px 2px 4px rgba(17, 24, 28, 0.04)",
+            }}
+          >
+            <div className="mr-base">
+              <IconBadge>
+                <ToolsSolidIcon />
+              </IconBadge>
+            </div>
+            <div>
+              <p className="font-semibold text-gray-700">
+                Check out beta features
+              </p>
+              <p className="text-xs">
+                Learn about hidden beta features and how to enable them in your store.
               </p>
             </div>
           </div>
