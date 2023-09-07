bea7211b24c50bf46b4d307b3cc5ad1911f78e5e
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2023-09-07 10:53:43 +0200

    Revert "medusa admin plugin was added"
    This reverts commit e6101039972fb48a0b0158bbf0bcdb9795287e3f.

--- medusa-config.js
+++ medusa-config.js
@@ -6,7 +6,7 @@ const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
-    process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL_TLS || process.env.REDIS_URL || "redis://localhost:6379";
@@ -17,40 +17,40 @@ const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
 
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
-    `medusa-fulfillment-manual`,
-    `medusa-payment-manual`,
-    {
-        resolve: "@medusajs/admin",
-        /** @type {import('@medusajs/admin').PluginOptions} */
-        options: {
-            autoRebuild: true,
-        },
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      // ...
     },
-    // Uncomment to add Stripe support.
-    // You can create a Stripe account via: https://stripe.com
-    // {
-    //   resolve: `medusa-payment-stripe`,
-    //   options: {
-    //     api_key: STRIPE_API_KEY,
-    //     webhook_secret: STRIPE_WEBHOOK_SECRET,
-    //   },
-    // },
+  },
+  // Uncomment to add Stripe support.
+  // You can create a Stripe account via: https://stripe.com
+  // {
+  //   resolve: `medusa-payment-stripe`,
+  //   options: {
+  //     api_key: STRIPE_API_KEY,
+  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
+  //   },
+  // },
 ];
 
 module.exports = {
-    projectConfig: {
-        redis_url: REDIS_URL,
-        // For more production-like environment install PostgresQL
-        database_url: DATABASE_URL,
-        database_type: "postgres",
-        // database_database: "./medusa-db.sql",
-        // database_type: "sqlite",
-        store_cors: STORE_CORS,
-        admin_cors: ADMIN_CORS,
-        database_extra:
-            process.env.NODE_ENV !== "development"
-                ? {ssl: {rejectUnauthorized: false}}
-                : {},
-    },
-    plugins,
+  projectConfig: {
+    redis_url: REDIS_URL,
+    // For more production-like environment install PostgresQL
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    // database_database: "./medusa-db.sql",
+    // database_type: "sqlite",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
+  },
+  plugins,
 };
