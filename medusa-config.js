b3ec8bc3340cf1af673aeb17760b111b3b3baab3
olivermrbl <oliver@mrbltech.com>
2023-10-06 18:57:23 -0700

    chore: Remove @medusajs/ui

--- medusa-config.js
+++ medusa-config.js
@@ -29,7 +29,7 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
