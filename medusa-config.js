6b73071735d7f5450a76bf7b8abdeb0f557a5f9d
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2024-01-04 15:19:29 +0100

    medusa-file-spaces plugin was added

--- medusa-config.js
+++ medusa-config.js
@@ -24,12 +24,12 @@ const plugins = [
     options: {
       spaces_url: process.env.SPACE_URL,
       bucket: process.env.SPACE_BUCKET,
-      region: process.env.SPACE_REGION,
       endpoint: process.env.SPACE_ENDPOINT,
       access_key_id: process.env.SPACE_ACCESS_KEY_ID,
       secret_access_key: process.env.SPACE_SECRET_ACCESS_KEY,
     },
   },
+
   // {
   //   resolve: "@medusajs/admin",
   //   /** @type {import('@medusajs/admin').PluginOptions} */
