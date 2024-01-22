4eed1cce1aa60372e1e692ce157822437b003da4
Tommy Parnell <tommy@terribledev.io>
2024-01-22 12:57:53 -0500

    turn off auto reload of admin page in production

--- medusa-config.js
+++ medusa-config.js
@@ -46,7 +46,7 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: true,
+      autoRebuild: process.env.NODE_ENV !== "production",
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
