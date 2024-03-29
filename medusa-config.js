e05e5afdee6c6a68e75ca2207135907f574cb3c5
Bertrand <bertrandk@outlook.com>
2024-03-29 17:30:56 -0400

    Configure algolia search

--- medusa-config.js
+++ medusa-config.js
@@ -52,6 +52,33 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: `medusa-plugin-algolia`,
+    options: {
+      applicationId: process.env.ALGOLIA_APP_ID,
+      adminApiKey: process.env.ALGOLIA_ADMIN_API_KEY,
+      settings: {
+        products: {
+          indexSettings: {
+            searchableAttributes: ["title", "description"],
+            attributesToRetrieve: [
+              "id",
+              "title",
+              "description",
+              "handle",
+              "thumbnail",
+              "variants",
+              "variant_sku",
+              "options",
+              "collection_title",
+              "collection_handle",
+              "images",
+            ],
+          },
+        },
+      },
+    },
+  },
 ];
 
 const modules = {
