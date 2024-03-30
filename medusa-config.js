0eae0958988772d6cc0f48f3c8c8848795cffe84
alex <alexauchart@gmail.com>
2024-03-30 15:52:50 +0100

    feat: readd admin

--- medusa-config.js
+++ medusa-config.js
@@ -44,11 +44,10 @@ const plugins = [
 		resolve: '@medusajs/admin',
 		/** @type {import('@medusajs/admin').PluginOptions} */
 		options: {
-			serve: process.env.NODE_ENV === 'development',
-			// autoRebuild: true,
-			// develop: {
-			//   open: process.env.OPEN_BROWSER !== 'false',
-			// },
+			autoRebuild: true,
+			develop: {
+				open: process.env.OPEN_BROWSER !== 'false',
+			},
 		},
 	},
 ];
