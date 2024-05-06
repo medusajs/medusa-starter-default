d032b2db9d5f8f8e92792f9d83be853964de2e64
Shahed Nasser <shahednasser@gmail.com>
2024-05-06 11:52:14 +0300

    remove old admin package

--- medusa-config.js
+++ medusa-config.js
@@ -42,16 +42,6 @@ const plugins = [
       upload_dir: "uploads",
     },
   },
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
-    },
-  },
 ];
 
 const modules = {
