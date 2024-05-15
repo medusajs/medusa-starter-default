ff47f131b3ee74167b0da79ce0055364abe35505
olivermrbl <oliver@mrbltech.com>
2024-05-15 17:22:10 +0200

    chore: Disable admin

--- medusa-config.js
+++ medusa-config.js
@@ -124,6 +124,9 @@ const projectConfig = {
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
   projectConfig,
+  admin: {
+    disable: true
+  },
   plugins,
   modules,
   featureFlags: {
