a6d27193aa5afa9c5862c5d7dad7c890e00ca38a
Shahed Nasser <shahednasser@gmail.com>
2024-05-30 13:27:53 +0300

    fix config

--- medusa-config.js
+++ medusa-config.js
@@ -3,12 +3,14 @@ import { loadEnv, defineConfig } from '@medusajs/utils'
 loadEnv(process.env.NODE_ENV, process.cwd())
 
 module.exports = defineConfig({
-  databaseUrl: process.env.DATABASE_URL,
-  http: {
-    storeCors: process.env.STORE_CORS,
-    adminCors: process.env.ADMIN_CORS,
-    authCors: process.env.AUTH_CORS,
-    jwtSecret: process.env.JWT_SECRET || "supersecret",
-    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
   }
 })
