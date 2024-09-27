c6fefe1ee3fbb1118c32fdfb308f28f1dafa8a0a
Harminder Virk <virk.officials@gmail.com>
2024-09-27 19:00:05 +0530

    chore: update packages and paths to new packages setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const { GracefulShutdownServer } = require("medusa-core-utils")
 
-const loaders = require("@medusajs/medusa/dist/loaders/index").default
+const loaders = require("@medusajs/medusa/loaders/index").default
 
 ;(async() => {
   async function start() {
