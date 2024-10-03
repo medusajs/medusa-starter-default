565bf79173eab0eba5ae627e4d2736aea1e6ef2d
Shahed Nasser <shahednasser@gmail.com>
2024-10-03 14:32:49 +0300

    disable admin

--- medusa-config.js
+++ medusa-config.js
@@ -12,5 +12,8 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
+  },
+  admin: {
+    disable: true
   }
 })
