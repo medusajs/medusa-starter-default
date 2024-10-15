4a9a70a7c344571e49b55c59a29ab07573907866
Harminder Virk <virk.officials@gmail.com>
2024-10-15 15:45:14 +0530

    feat: get in sync with the feat/v2 branch

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,14 +1,14 @@
-const { loadEnv, defineConfig } = require('@medusajs/framework/utils')
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
