f3b0dbf28937ab9557a8b14af5ae59e4414acd29
olivermrbl <oliver@mrbltech.com>
2024-11-26 09:52:25 +0100

    chore: Use js for medusa config

---
+++ medusa-config.js
@@ -0,0 +1,19 @@
+const { loadEnv, defineConfig } = require("@medusajs/framework/utils");
+
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  admin: {
+    disable: true,
+  },
+});
