f6e901dbc9840decd61632f05090744bf48268ab
Max-of-web <75911580+Max-of-web@users.noreply.github.com>
2025-04-23 01:44:37 +0300

    plugins

--- medusa-config.ts
+++ medusa-config.ts
@@ -22,8 +22,10 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  plugins, // ✅ This is the missing piece
 })
+
 
 
 
