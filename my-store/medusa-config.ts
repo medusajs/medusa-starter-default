71553c60c3d36aa218a2f991ae0f7b220aa34f16
Max-of-web <75911580+Max-of-web@users.noreply.github.com>
2025-04-23 03:03:49 +0300

    fd

--- my-store/medusa-config.ts
+++ my-store/medusa-config.ts
@@ -1,21 +1,22 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "production", process.cwd())
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    workerMode: "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
-      redisUrl: process.env.REDIS_URL,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
-    admin: {
-      disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    }
-  }
+  },
+  admin: {
+    disable: true
+  },
 })
 
