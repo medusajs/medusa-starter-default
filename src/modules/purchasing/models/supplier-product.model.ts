01aadb925b89de7fc74de7907ecbe770c7b660d4
Pcoq <109622059+Pcoq@users.noreply.github.com>
2025-07-11 08:42:02 +0200

    fix purchasing migration

--- src/modules/purchasing/models/supplier-product.model.ts
+++ src/modules/purchasing/models/supplier-product.model.ts
@@ -4,6 +4,6 @@ export default model.define("supplier_product", {
   id: model.id().primaryKey(),
   supplier_id: model.id(),
   product_variant_id: model.id(),
-  supplier_sku: model.text().optional(),
+  supplier_sku: model.text().nullable(),
   price: model.bigNumber(),
 }) 
--- src/modules/purchasing/models/supplier.model.ts
+++ src/modules/purchasing/models/supplier.model.ts
@@ -3,5 +3,5 @@ import { model } from "@medusajs/utils"
 export default model.define("supplier", {
   id: model.id().primaryKey(),
   name: model.text(),
-  email: model.text().optional(),
+  email: model.text().nullable(),
 }) 
