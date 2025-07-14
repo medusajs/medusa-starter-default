ad73686659e4ef87730d0b329df76fe229c8464c
Pcoq <hannesdepauw@hotmail.com>
2025-07-14 12:37:36 +0200

    revert bad config

--- medusa-config.ts
+++ medusa-config.ts
@@ -13,10 +13,6 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
   },
-  admin: {
-    path: "/app",
-    backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
-  },
   modules: [
     {
       resolve: "./src/modules/purchasing",
