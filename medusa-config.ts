1860d6a33eb8bdb60278abd1f8417d09e3c2b4a3
Luka Giorgadze <luka.giorgadze@gmail.com>
2025-10-21 17:44:28 +0400

    wip

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,4 +1,5 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+import { defineConfig, loadEnv } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
@@ -8,9 +9,80 @@ module.exports = defineConfig({
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
+      // @ts-expect-error: vendorCors is not a valid config
+      vendorCors: process.env.VENDOR_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret'
     }
-  }
+  },
+  plugins: [
+    {
+      resolve: '@mercurjs/b2c-core',
+      options: {}
+    },
+    {
+      resolve: '@mercurjs/commission',
+      options: {}
+    },
+    {
+      resolve: '@mercurjs/algolia',
+      options: {
+        apiKey: process.env.ALGOLIA_API_KEY,
+        appId: process.env.ALGOLIA_APP_ID
+      }
+    },
+    {
+      resolve: '@mercurjs/reviews',
+      options: {}
+    },
+    {
+      resolve: '@mercurjs/requests',
+      options: {}
+    },
+    {
+      resolve: '@mercurjs/resend',
+      options: {}
+    }
+  ],
+  modules: [
+    {
+      resolve: '@medusajs/medusa/payment',
+      options: {
+        providers: [
+          {
+            resolve:
+              '@mercurjs/payment-stripe-connect/providers/stripe-connect',
+            id: 'stripe-connect',
+            options: {
+              apiKey: process.env.STRIPE_SECRET_API_KEY
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: '@medusajs/medusa/notification',
+      options: {
+        providers: [
+          {
+            resolve: '@mercurjs/resend/providers/resend',
+            id: 'resend',
+            options: {
+              channels: ['email'],
+              api_key: process.env.RESEND_API_KEY,
+              from: process.env.RESEND_FROM_EMAIL
+            }
+          },
+          {
+            resolve: '@medusajs/medusa/notification-local',
+            id: 'local',
+            options: {
+              channels: ['feed', 'seller_feed']
+            }
+          }
+        ]
+      }
+    }
+  ]
 })
