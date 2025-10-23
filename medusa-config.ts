42dafe2bf6d6b9c24295788c0657ff20f3e717aa
Luka Giorgadze <luka.giorgadze@gmail.com>
2025-10-23 16:18:27 +0400

    wip

--- medusa-config.ts
+++ medusa-config.ts
@@ -7,11 +7,11 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
+      storeCors: "https://shopcat-vendorpanel-staging-575553836979.europe-west1.run.app",
+      adminCors: "https://shopcat-vendorpanel-staging-575553836979.europe-west1.run.app",
       // @ts-expect-error: vendorCors is not a valid config
-      vendorCors: process.env.VENDOR_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      vendorCors: "https://shopcat-vendorpanel-staging-575553836979.europe-west1.run.app",
+      authCors: "https://shopcat-vendorpanel-staging-575553836979.europe-west1.run.app",
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret'
     }
